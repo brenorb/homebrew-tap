65de86028c31df6f515c0e5469f55ba579392d97
Breno Brito <brenorb@gmail.com>
2026-06-18 17:20:32 -0300

    fix: pin fast-transcript formula version

--- Formula/fast-transcript.rb
+++ Formula/fast-transcript.rb
@@ -1,6 +1,7 @@
 class FastTranscript < Formula
   desc "Fast local transcription for large lectures with NVIDIA Parakeet ONNX"
   homepage "https://github.com/brenorb/fast-transcript"
+  version "1.1.2"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
